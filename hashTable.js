a8959abe0ee17497459e8340596bff7d513e3f34
omonmunduz <omurbekn@gmail.com>
2021-01-24 22:03:33 -0800

    hash table implementation using arrays

---
+++ hashTable.js
@@ -0,0 +1,67 @@
+/** Class representing a Hash Table */
+
+class HashTable {
+    constructor(val) {
+      this._storage = [];
+      this._tableSize = val;
+    }
+    /*
+    * Inserts a new key-value pair
+    * @param {string} key - the key associated with the value
+    * @param {*} value - the value to insert
+    */
+    insert(key,value) {
+      const index = this._hash(key, this._tableSize);
+      if(!this._storage[index]) {
+        this._storage[index] = [];
+      }
+      this._storage[index].push([key, value])
+    }
+    /*
+    * Deletes a key-value pair
+    * @param {string} key - the key associated with the value
+    * @return {*} value - the deleted value
+    */
+    remove(key) {
+      const index = this._hash(key, this.tableSize);
+      const arrayAtIndex = this._storage[index];
+  
+    }
+    /*
+    * Returns the value associated with a key
+    * @param {string} key - the key to search for
+    * @return {*} - the value associated with the key
+    */
+    retrieve(key) {
+      const index = this._hash(key,this._tableSize);
+      const arrayAtIndex = this._storage[index];
+  
+    if(arrayAtIndex){
+         for(let i=0;i<arrayAtIndex.length; i++){
+           if(arrayAtIndex[i][0] == key){
+             return arrayAtIndex[i][1]
+           }
+      }
+    }
+    }  
+    /*
+    * Hashes string value into an integer that can be mapped to an array index
+    * @param {string} str - the string to be hashed
+    * @param {number} n - the size of the storage array
+    * @return {number} - an integer between 0 and n
+    */
+    _hash(str, n) {
+      let sum = 0;
+      for (let i = 0; i < str.length; i++)
+          sum += str.charCodeAt(i) * 3
+      return sum % n;
+    }
+  }
+  const myTable = new HashTable(25)
+  console.log(myTable)
+  myTable.insert('a',1)
+  myTable.insert('b',2)
+  myTable.insert('c',3)
+  console.log(myTable)
+  console.log(myTable.retrieve('a'))
+  
